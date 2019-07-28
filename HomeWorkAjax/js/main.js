2c536747a05f0e384e3204b0d84ac31e9b0edb5c
IlliaMatiash <matas6151@gmail.com>
2019-07-29 01:10:37 +0300

    new version home work Ajax

--- HomeWorkAjax/js/main.js
+++ HomeWorkAjax/js/main.js
@@ -1,74 +1,48 @@
-// get users -> 
-// GET https://jsonplaceholder.typicode.com/users?search=Ihor&limit=20
-// get user -> GET https://jsonplaceholder.typicode.com/users?id=212312
-// get user -> GET https://jsonplaceholder.typicode.com/users/212312
-// create user -> POST https://jsonplaceholder.typicode.com/users
-// update user -> PUT https://jsonplaceholder.typicode.com/users/212312
-// delete user -> DELETE https://jsonplaceholder.typicode.com/users/212312
-
-// path variable / query params
-
-function swiperAll(){
-    let swiper = new Swiper('.swiper-container', {
-      effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      coverflowEffect: {
-        rotate: 40,
-        stretch: 0,
-        depth: 300,
-        modifier: 1,
-        slideShadows : true,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    });
-
-
-}
 const API = 'https://test-users-api.herokuapp.com/';
 let allUsers = [];
-//const container = document.querySelector('.users');
-const swiperWrapper = document.querySelector('.swiper-wrapper');
+const carousel = document.querySelector('.carousel');
 const nameEl = document.querySelector('#name');
 const ageEl = document.querySelector('#age');
-//const idEl = document.querySelector('#id');
 const btnCreate = document.querySelector('#create');
 
 btnCreate.addEventListener('click', () => {
-    
-    console.log(allUsers.data[allUsers.data.length-1].id);
-//    if(allUsers.data.length-1 === 9){
-//        if(allUsers.data.length-2 === 9 && allUsers.data.length-1 === 9){
-//            idEl = allUsers.splice(0 ,allUsers.data.length-2) + "100"; 
-//        }
-//        const lastNumb = Number(allUsers.data.length-1 +1).toString();
-//        idEl = allUsers.splice(0 ,allUsers.data.length-1) + lastNumb;
-//        console.log(idEl);
-//    }
     const user = {
-    name: nameEl.value,
-    age: ageEl.value
+        name: nameEl.value,
+        age: ageEl.value
     };
-    fetch(API + "users", {
-        method: "POST",
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    })
-    
-    .then((data) => {
-//      user.id = data.id;
-        
-      allUsers.data.push(user);
-      renderUsers();
-    }).catch(err => {
-        console.log(err);
-    })
+    if (nameEl.value != "" && ageEl.value != "") {
+        fetch(API + "users", {
+            method: "POST",
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then((data) => {
+          allUsers.data.push(user);
+          renderUsers();
+        }).catch(err => {
+            console.log(err);
+        });
+    nameEl.value = "";
+    ageEl.value = "";
+    }
 })
+
+async function changeValue(id, userNameInput, userAgeInput) {
+    const user = {
+        name: userNameInput,
+        age: userAgeInput
+    };
+    await fetch(API + 'users/' + id, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    }).then((data) => {
+        allUsers.data.name = user.name;
+        allUsers.data.age = user.age;
+    })
+}
 
 function getUsers() {
     return fetch(API + 'users').then(res => {
@@ -78,89 +52,85 @@ function getUsers() {
         console.log('Cant get users' ,err);
     })
 }
+
 async function deleteUser(userId) {
-    swiperAll();
     await fetch(API + 'users/' + userId, {
         method: 'DELETE'
     })
     allUsers.data = allUsers.data.filter((user) => user.id !== userId);
     renderUsers();
-    
 }
-//
-//
-//async function btnSave(userId) {
-//        inputName.value = user.name;
-//        inputAge.value = user.age;
-//    await fetch(API + 'users/' + userId, {
-//        method: 'PUT',
-//        headers: {
-//        'Content-Type': 'application/json'
-//        },
-//        body: JSON.stringify(user)
-//    })
-////    allUsers.data = allUsers.data.filter((user) => user.id !== userId);
-//    renderUsers();
-//}
+
 function renderUsers(){
-    
-    swiperWrapper.innerHTML = '';
+    carousel.innerHTML = '';
     allUsers.data.forEach((user) => {
-        const swiperSlide = document.createElement('div');
-        swiperSlide.className = 'swiper-slide';
+        
+        const carouseliItem = document.createElement('div');
+        carouseliItem.className = 'carousel-item';
         
         const card = document.createElement('div');
-        card.className = 'card';
+        card.className = 'row';
         
-        const sliderText = document.createElement('div');
-        sliderText.className = 'sliderText';
-        sliderText.innerHTML = `<h3>Persons card</h3>`;
+        const aboutPerson = document.createElement('div');
+        aboutPerson.className = 'aboutPerson';
         
-        const content = document.createElement('div');
-        content.className = 'content';
+        const buttons = document.createElement('div');
+        buttons.className = 'buttons';
         
-//        const inputName = document.createElement('input');
-//        const inputAge = document.createElement('input');
-//        inputName.value = user.name;
-//        inputAge.value = user.age;
-        content.innerHTML = `
-            <h4>${user.name}</h4>
-            <h5>${user.age}</h5>
-        `
-        const btn = document.createElement('button');
-        btn.textContent = "X";
+        const btnSave = document.createElement('button');
+        btnSave.className = 'btn';
+        btnSave.textContent = "SAVE";
         
-        btn.addEventListener('click', () => {
+        const btnDelete = document.createElement('button');
+        btnDelete.className = 'btn';
+        btnDelete.textContent = "DELETE";
+        
+        const nameInput = document.createElement('input');
+        nameInput.defaultValue = `${user.name}`;
+        
+        const ageInput = document.createElement('input');
+        ageInput.defaultValue = `${user.age}`;
+
+        btnDelete.addEventListener('click', () => {
             deleteUser(user.id);
         })
         
-//        const btnSave = document.createElement('button');
-//        btnSave.textContent = "Save";
+         btnSave.addEventListener('click', () => {
+            changeValue(user.id, nameInput.value, ageInput.value);
+        });
         
-//        btn.addEventListener('click', () => {
-//            saveChange(user.id);
-//        })
-        btn.className = 'user__remove'; 
-//        div.append(inputName);
-//        div.append(inputAge);
-//        div.append(btnSave);
-        card.append(sliderText);
-        card.append(content);
-        swiperSlide.append(card);
-        swiperSlide.append(btn);
-        swiperWrapper.append(swiperSlide);
-    })
+        aboutPerson.append(nameInput);
+        aboutPerson.append(ageInput);
+        
+        card.append(aboutPerson);
+        buttons.append(btnSave);
+        buttons.append(btnDelete);
+        card.append(buttons);
+        
+        carouseliItem.append(card);
+        carousel.append(carouseliItem);
+    }) 
+    $(document).ready(function(){
+        $('.carousel').carousel();
+  });
 }
 
 getUsers().then(data => {
     allUsers = data;
-    
     console.log(allUsers);
-   
     renderUsers(allUsers);
-    swiperAll();
     
 })
+
+
+
+
+
+
+
+
+
+
 
 
 
